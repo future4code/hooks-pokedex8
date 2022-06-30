ffe2ed671999426cfcc8972a6db903e3ba783a7c
WictorFeitosa <wictor.feitosa@gmail.com>
2022-06-29 21:39:16 -0300

    axios na api

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,12 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router";
 import styled from "styled-components";
 import { goToPokedexPage } from "../../Router/coordinator";
 import { Button } from "@material-ui/core";
 import Typography from "@material-ui/core/Typography";
+import axios from "axios"
+
 
 export default function HomePage() {
   const navigate = useNavigate();
+  const [pokemon , setPokemon] = useState()
+
+  useEffect(() => {
+    axios.get(`https://pokeapi.co/api/v2/pokemon?limit=20&offset=0`)
+    .then((res) => {
+        alert("Aplicação enviada com sucesso!")
+        setPokemon(res.data)
+        clear()
+    })
+    .catch((err) => alert(err.response.message))
+  },[])
+
+
 
   return (
     <div>
