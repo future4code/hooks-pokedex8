4c2a37eedfd61c4f135e13cd5660c9711fb91659
JonasLevy <jonaslevy0408@gmail.com>
2022-07-02 17:42:51 -0300

    criei o cardPokemon e renderizei

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,43 +1,57 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router";
-import styled from "styled-components";
 import { goToPokedexPage } from "../../Router/coordinator";
 import { Button } from "@material-ui/core";
 import Typography from "@material-ui/core/Typography";
 import axios from "axios";
+import { ContainerHome } from './styles'
+import logoimg from '../../imagens/pokedex.png'
+import CardPokemon from "../../components/CardPokemon";
+
 
 
 export default function HomePage() {
   const navigate = useNavigate();
-  const [pokemon , setPokemon] = useState()
+  const [pokemon, setPokemon] = useState()
 
   useEffect(() => {
-    axios.get(`https://pokeapi.co/api/v2/pokemon?offset=20&limit=20`)
-    .then((res) => {
+    axios.get(`https://pokeapi.co/api/v2/pokemon?offset=0&limit=20`)
+      .then((res) => {
         console.log("Aplicação enviada com sucesso!")
         setPokemon(res.data.results)
-    })
-    .catch((err) => alert(err))
-  },[])
+      })
+      .catch((err) => alert(err))
+  }, [])
 
-    const Log = () =>{
-      console.log(pokemon)
-    }
+  const Log = () => {
+    console.log(pokemon)
+  }
 
   return (
-    <div>
-      <Typography variant="h1" align={"center"}>
-        Pokedex 8 - Home Page
-      </Typography>
-      <button onClick={()=>Log()}>console</button>
-      <Button
-        variant="contained"
-        color="primary"
-        onClick={() => goToPokedexPage(navigate)}
-      >
-        Pokedex Page
-      </Button>
-      
-    </div>
+    <ContainerHome >
+      <header>
+        <img src={logoimg} />
+        <button onClick={() => Log()}>console</button>
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={() => goToPokedexPage(navigate)}
+        >
+          Pokedex Page
+        </Button>
+      </header>
+
+      <section className="sectionCardRendered">
+
+      {pokemon && pokemon.map(objPoke=>{
+
+        return(
+          <CardPokemon pokemon={objPoke} />
+        )
+      })}
+
+      </section>
+
+    </ContainerHome>
   );
 }
